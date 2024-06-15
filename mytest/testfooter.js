77bb5ca5d93dc2223e28ff58105a3c2f7c2e45ff
suan318 <yixuanwang0129@gmail.com>
2024-06-15 21:24:59 +0800

    試做footer

---
+++ mytest/testfooter.js
@@ -0,0 +1,18 @@
+// Show the button when the user scrolls down 100px from the top of the document
+window.onscroll = function () {
+    var button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+// Smooth scroll to top function
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
