8d00c13fe5407bcce9f56a49cf39be3c35539164
Sophie0318 <solophie34@gmail.com>
2024-06-16 20:28:20 +0800

    react-layout

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <main className="h-100 d-flex flex-column justify-content-center align-items-center">
+        <h1>Scroll Down</h1>
+        <p>Some content...</p>
+      </main>
+    </>
+  )
+}
