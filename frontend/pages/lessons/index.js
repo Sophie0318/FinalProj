c68e62f637afa88a09ffb4652c7743c53794ad5c
Leana <yanning0113niao@gmail.com>
2024-06-25 17:23:35 +0800

    課程列表頁

--- frontend/pages/lessons/index.js
+++ frontend/pages/lessons/index.js
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react'
 import Layout2 from '@/components/layout/layout2'
 
 export default function Index() {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/articles/api')
+      .then((r) => r.json())
+      .then((data) => {
+        setArticles(data.rows)
+      })
+  }, [])
   return (
     <>
       <Layout2 title="課程列表" pageName="lessons">
-        <section>124</section>
+        123
       </Layout2>
     </>
   )
