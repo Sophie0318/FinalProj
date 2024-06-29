b4fe4827738ab2399fa426f2099b06d5d397a2be
yyuntzpan <yap0602@gmail.com>
2024-06-29 21:19:00 +0800

    [edit]switch按鈕的外觀完成

---
+++ frontend/components/common/switch/switch.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './switch.module.css'
+
+export default function Switch({ isOn, handleToggle }) {
+  return (
+    <>
+      <input
+        checked={isOn}
+        onChange={handleToggle}
+        className={styles.switchCheckbox}
+        id={`switch-old`}
+        type="checkbox"
+      />
+      <label
+        style={{ background: isOn && '#19394A' }}
+        className={styles.switchLabel}
+        htmlFor={`switch-old`}
+      >
+        <span className={styles.switchButton}></span>
+      </label>
+    </>
+  )
+}
