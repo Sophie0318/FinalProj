1f506cbbe6c25f02c2ced6692b5ddc7286c12880
yyuntzpan <yap0602@gmail.com>
2024-06-30 17:58:31 +0800

    [edit] 一些格式小變動

--- frontend/components/common/searchbar/searchbar.js
+++ frontend/components/common/searchbar/searchbar.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { IoSearch } from 'react-icons/io5'
 import styles from './searchbar.module.css'
-//TODO 讓搜尋元件可以傳入 placeholder, height, MaxWindth, backgroundColor
-export default function SearchBar({ placeholder = "請輸入地址搜尋...", maxWidth = "600px" }) {
+//TODO 讓搜尋元件可以傳入 placeholder, height(size), MaxWindth, backgroundColor
+export default function SearchBar({
+  placeholder = '請輸入地址搜尋...',
+  maxWidth = '600px',
+  size = '60px',
+  // 增加props mainColor or 設定Theme
+}) {
   return (
     <div className={styles.search} style={{ maxWidth }}>
-      <div className={styles.searchIcon}>
+      <div
+        className={styles.searchIcon}
+        style={{
+          width: size,
+          height: size,
+        }}
+      >
         <IoSearch />
       </div>
       <input
         type="text"
         name="search_input"
         className={styles.search_input}
+        style={{
+          height: size,
+          paddingLeft: `calc(${size} + 9px)`,
+        }}
         placeholder={placeholder}
       />
     </div>
