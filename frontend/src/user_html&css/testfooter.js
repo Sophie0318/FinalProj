946ab49c747bf503773f37c426c764b4e519eaab
suan318 <yixuanwang0129@gmail.com>
2024-06-30 22:47:06 +0800

    改了不知道甚麼

--- frontend/src/user_html&css/testfooter.js
+++ frontend/src/user_html&css/testfooter.js
@@ -1,18 +1,16 @@
-// Show the button when the user scrolls down 100px from the top of the document
 window.onscroll = function () {
-    var button = document.getElementById("back-to-top");
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-};
+  var button = document.getElementById('back-to-top')
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    button.style.display = 'block'
+  } else {
+    button.style.display = 'none'
+  }
+}
 
 // Smooth scroll to top function
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
 }
-
