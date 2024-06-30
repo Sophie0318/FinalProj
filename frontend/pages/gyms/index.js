0461afdbde56d04fd6cefd8d5c58fd92f0806eaa
yyuntzpan <yap0602@gmail.com>
2024-06-30 14:25:23 +0800

    [edit]設定switch的布林值為開啟狀態

--- frontend/pages/gyms/index.js
+++ frontend/pages/gyms/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout2 from '@/components/layout/layout2'
 import styles from './gyms.module.css'
 import SearchBar from '@/components/common/searchbar/searchbar'
-import Toggle from '@/components/common/toggle/toggle'
+import Switch from '@/components/common/switch/switch'
 
-export default function FindGyms() {
+export default function Gyms() {
   // 功能寫在這
+  //switch 的state
+  const [boo, setBoo] = useState(true)
   return (
-    <Layout2 title="尋找場館" pageName="gyms">  
+    <Layout2 title="尋找場館" pageName="gyms">
       <div className={styles.containerFixedP80}>
         <div className="row">
-          <div className={styles.mt50}>
-            <SearchBar  placeholder="輸入地址查詢最近場館..." maxWidth="789px" />
-            <Toggle/>
+          <div className={`${styles.mt50} ${styles.rt50}`}>
+            <div
+              className={
+                'd-flex flex-row justify-content-between align-items-center'
+              }
+            >
+              <SearchBar
+                placeholder="輸入地址查詢最近場館..."
+                maxWidth="789px"
+              />
+              <Switch isOn={boo} handleToggle={() => setBoo(!boo)} />
+            </div>
           </div>
         </div>
       </div>
