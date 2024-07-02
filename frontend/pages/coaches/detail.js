0a8782dcccf97e8191d7c5e3204d781d4665c4df
Leana <yanning0113niao@gmail.com>
2024-07-02 12:08:17 +0800

    [0702 12:07] 收藏、預約btn完成 可以變色

--- frontend/pages/coaches/detail.js
+++ frontend/pages/coaches/detail.js
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from 'react'
 import Layout2 from '@/components/layout/layout2'
 import Carousel from '@/components/carousel'
 import styles from '@/styles/coachDetail.module.css'
+import { IoCall, IoHeart } from 'react-icons/io5'
 
 export default function Detail() {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked(!isClicked)
+  }
+
   return (
     <>
       <Layout2 title="教練列表" pageName="coaches">
@@ -22,6 +29,21 @@ export default function Detail() {
                 <p className={styles.coachDesc}>
                   我們的專業教練李安妮專精於心肺功能訓練和增肌運動，特別針對銀髮族設計課程。安妮教練擁有多年經驗，致力於幫助長者提升體能和增強肌肉力量。她溫柔細心，善於調整訓練計劃，確保每位長者都能在安全的環境中運動。加入我們，與安妮教練一起，享受健康活力的生活！
                 </p>
+              </div>
+              <div className={styles.btn}>
+                <button className={`${styles.btnLike}`} onClick={handleClick}>
+                  <span
+                    className={`${styles.icon} ${
+                      isClicked ? styles.clicked : ''
+                    }`}
+                  >
+                    <IoHeart />
+                  </span>
+                  <span>收藏</span>
+                </button>
+                <button className={styles.btnReserve}>
+                  <IoCall /> 預約
+                </button>
               </div>
             </div>
           </div>
