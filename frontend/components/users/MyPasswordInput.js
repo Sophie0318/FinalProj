bd8fc3bb8051eb6a7071f7d6efe0a5efd08f9671
suan318 <yixuanwang0129@gmail.com>
2024-07-06 22:45:16 +0800

    調整

--- frontend/components/users/MyPasswordInput.js
+++ frontend/components/users/MyPasswordInput.js
@@ -6,6 +6,8 @@ import { FaEye, FaEyeSlash, FaExclamationCircle } from 'react-icons/fa'
 const MyPasswordInput = ({ password, setPassword }) => {
   //一開始不秀密碼
   const [showPassword, setShowPassword] = useState(false)
+  //一開始不秀icon
+  const [showIcon, setShowIcon] = useState(null)
   //一開始沒有error message
   const [errorMessage, setErrorMessage] = useState('')
 
@@ -18,6 +20,7 @@ const MyPasswordInput = ({ password, setPassword }) => {
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value
     setPassword(newPassword)
+    setShowIcon(newPassword.length > 0) // 有輸入密碼就顯示icon
 
     if (newPassword.length < 3) {
       setErrorMessage('請填入至少3位以上的密碼')
@@ -42,9 +45,11 @@ const MyPasswordInput = ({ password, setPassword }) => {
           onChange={handlePasswordChange}
           placeholder="請輸入您的密碼"
         />
-        <div className={styles.myicon} onClick={togglePasswordVisibility}>
-          {showPassword ? <FaEyeSlash /> : <FaEye />}
-        </div>
+        {showIcon && (
+          <div className={styles.myicon} onClick={togglePasswordVisibility}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        )}
         {errorMessage && (
           <div className={styles.erro_message}>
             <p className={styles.tomatoP}>{errorMessage}</p>
