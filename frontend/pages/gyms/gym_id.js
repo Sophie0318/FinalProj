2aec7f63ab87db5129f8bcc59b6ca8d52c413015
yyuntzpan <yap0602@gmail.com>
2024-07-08 18:22:57 +0800

    [edit] 基本手機details排版

--- frontend/pages/gyms/gym_id.js
+++ frontend/pages/gyms/gym_id.js
@@ -22,36 +22,58 @@ const gymsData = [
       '/gym1.jpg',
     ],
     phoneNumber: '02-1234-5678',
+    information:
+      '力量源練身館座落於北投市中心，是一間專業健身、體能訓練所在於其中一個的活化空間場館。我們科技力光合有機研發耳機系統，令大家在選擇的時候品質無需擔心。更新科技化新式健身器材設施讓您操作時更為輕鬆......',
+    price: [
+      '月卡會員: NT$ 1,900/月',
+      '季卡會員: NT$ 4,000/月 (每月約1,333)',
+      '半年卡會員: NT$ 6,000/月 (每月約1,167)',
+      '單次體驗: NT$ 300/次',
+    ],
+    equipment: [
+      '有氧運動區：配備多部跑步機、橢圓機、單車等',
+      '團體課教室：舉辦各種團體有氧課程',
+      '個人訓練區：可進行一對一的個人訓練',
+      '更衣室和淋浴設施：提供舒適的更衣和盥洗空間',
+    ],
   },
 ]
 export default function GymDetail({ gymId }) {
   // 假設 gymId 是從 1 開始的，我們需要減 1 來獲取正確的索引
-  const gym = gymsData[gymId - 1] || gymsData[0];
-
+  const gym = gymsData[gymId - 1] || gymsData[0]
 
   return (
     <div>
       <Layout3>
         <div title="title-box" className={styles.container}>
           <h3> {gym.name} </h3>
-          <h6>subtitle</h6>
+          <h6>{gym.subtitle}</h6>
         </div>
 
         <div className={styles.imgContainer}>
-          {/* map第一張 */}
+          {/* 預設第一張 或 點選下方縮圖切換照片*/}
           <img src="/gym1.jpg" alt="場館內部" className={styles.coverImg} />
 
           <div className={styles.imgGroup}>
-            {/* 使用map第二張到最後 */}
-            <img src="/gym1.jpg" alt="場館內部" className={styles.otherImg} />
-            <img src="/gym1.jpg" alt="場館內部" className={styles.otherImg} />
+            {gym.images.map((image, i) => (
+              <img
+                key={i}
+                src={image}
+                alt="場館內部"
+                className={styles.otherImg}
+              />
+            ))}
+           
+           
           </div>
         </div>
 
         <div className="container">
-          <h6>{gym.address}</h6>
-          <h6>{gym.phoneNumber}</h6>
-          <Badges />
+          <div className={styles.px8}>
+            <h6>{gym.address}</h6>
+            <h6>{gym.phoneNumber}</h6>
+            <Badges />
+          </div>
 
           <div className={styles.btn}>
             <button className={`${styles.btnLike}`}>
@@ -66,28 +88,26 @@ export default function GymDetail({ gymId }) {
           </div>
 
           <div className={styles.details}>
-            <h5>場館介紹</h5>
-            <p>
-              力量源練身館座落於北投市中心，是一間專業健身、體能訓練所在於其中一個的活化空間場館。我們科技力光合有機研發耳機系統，令大家在選擇的時候品質無需擔心。更新科技化新式健身器材設施讓您操作時更為輕鬆......
-            </p>
-
-            <h5>收費</h5>
-            <ul>
-              <li>月卡會員: NT$ 1,900/月</li>
-              <li>季卡會員: NT$ 4,000/月 (每月約1,333)</li>
-              <li>半年卡會員: NT$ 6,000/月 (每月約1,167)</li>
-              <li>單次體驗: NT$ 300/次</li>
-            </ul>
-            <p>特別優惠: 新客體驗課程買一送一，詳情請洽詢專人。</p>
-
-            <h5>場館設備</h5>
-            <ol>
-              <li>有氧運動區：配備多部跑步機、橢圓機、單車等</li>
-              <li>力量訓練區：提供完整的肌力訓練器材</li>
-              <li>團體課教室：舉辦各種團體有氧課程</li>
-              <li>個人訓練區：可進行一對一的個人訓練</li>
-              <li>更衣室和淋浴設施：提供舒適的更衣和盥洗空間</li>
-            </ol>
+            <div>
+              <h5 className={styles.h5}>場館介紹｜</h5>
+              <p>{gym.information}</p>
+            </div>
+            <div>
+              <h5 className={styles.h5}>收費｜</h5>
+              <ul className="p-font">
+                {gym.price.map((price, i) => (
+                  <li key={i}>{price}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h5 className={styles.h5}>場館設備｜</h5>
+              <ol className="p-font">
+                {gym.equipment.map((equipment, i) => (
+                  <li key={i}>{equipment}</li>
+                ))}
+              </ol>
+            </div>
           </div>
         </div>
       </Layout3>
