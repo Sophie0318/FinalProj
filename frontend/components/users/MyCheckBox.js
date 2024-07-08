0601cb51561e49923b5b905fa7324a157c83cbe1
suan318 <yixuanwang0129@gmail.com>
2024-07-08 10:39:34 +0800

    改到一半

--- frontend/components/users/MyCheckBox.js
+++ frontend/components/users/MyCheckBox.js
@@ -3,7 +3,6 @@ import styles from '../../styles/mycheckbox.module.css'
 export default function MyCheckBox() {
   return (
     <div className={styles.filter}>
-      <p className={styles.select}>請選擇類別 ｜</p>
       <div className={styles.checkboxWrapper}>
         <div className={styles.checkboxes}>
           <input
@@ -12,37 +11,7 @@ export default function MyCheckBox() {
             name="aerobic"
             className={styles.checkbox}
           />
-          <label htmlFor="aerobic">有氧</label>
-        </div>
-
-        <div className={styles.checkboxes}>
-          <input
-            type="checkbox"
-            id="stretching"
-            name="stretching"
-            className={styles.checkbox}
-          />
-          <label htmlFor="stretching">伸展</label>
-        </div>
-
-        <div className={styles.checkboxes}>
-          <input
-            type="checkbox"
-            id="muscle"
-            name="muscle"
-            className={styles.checkbox}
-          />
-          <label htmlFor="muscle">增肌</label>
-        </div>
-
-        <div className={styles.checkboxes}>
-          <input
-            type="checkbox"
-            id="weight"
-            name="weight"
-            className={styles.checkbox}
-          />
-          <label htmlFor="weight">重訓</label>
+          <label htmlFor="aerobic">記住我</label>
         </div>
       </div>
     </div>
