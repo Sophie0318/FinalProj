f9aae163e28549549d8507a7c47b719f1f760b68
sophie0318 <solophie34@gmail.com>
2024-07-12 22:38:43 +0800

    frontend index hero section fadeout animation

--- frontend/components/layout/layout1.js
+++ frontend/components/layout/layout1.js
@@ -4,13 +4,13 @@ import Footer from '../common/footer'
 import Head from 'next/head'
 import BackToTop from '../common/buttons/back-to-top'
 
-export default function Layout1({ children, title = '' }) {
+export default function Layout1({ children, title = '', hideLogo = false }) {
   return (
     <>
       <Head>
         <title>{title ? title : ''}</title>
       </Head>
-      <Navbar />
+      <Navbar hideLogo={hideLogo} />
       <div className={`d-flex flex-column`}>{children}</div>
       <Footer />
       <BackToTop />
