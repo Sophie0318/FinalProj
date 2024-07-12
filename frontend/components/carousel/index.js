3e71818d1e5aa7a06c3fe683cc4fde4982ca1763
Leana <yanning0113niao@gmail.com>
2024-07-12 16:03:06 +0800

    更新輪播元件

--- frontend/components/carousel/index.js
+++ frontend/components/carousel/index.js
@@ -2,10 +2,7 @@ import React, { useEffect } from 'react'
 import { FaArrowLeftLong, FaArrowRightLong } from 'react-icons/fa6'
 import styles from './carousel.module.css'
 
-export default function Carousel({
-  carouselTitle = '熱門選項',
-  data = ['card1', 'card2', 'card3'],
-}) {
+export default function Carousel({ carouselTitle = '熱門選項', data = [] }) {
   const list = ['card1', 'card2', 'card3']
 
   // TODO: bootstrap 輪播覆蓋也很難做出一模一樣的效果, 考慮直接用kacco網站用的https://swiperjs.com/
@@ -20,28 +17,29 @@ export default function Carousel({
         <div className="col-md-9 ps-md-4 ps-sm-3">
           <div id="carousel" className="carousel slide" data-bs-wrap="false">
             <div className="carousel-inner">
-              {data.map((v, i) => {
-                return (
-                  <div
-                    key={i}
-                    className={
-                      i === 0 ? 'carousel-item active' : 'carousel-item'
-                    }
-                  >
+              {data &&
+                data.map((v, i) => {
+                  return (
                     <div
-                      className="d-flex justify-content-center align-items-center"
-                      style={{
-                        height: '400px',
-                        width: '504px',
-                        backgroundColor: 'var(--color-light-gray)',
-                        borderRadius: '50px',
-                      }}
+                      key={i}
+                      className={
+                        i === 0 ? 'carousel-item active' : 'carousel-item'
+                      }
                     >
-                      {v}
+                      <div
+                        className="d-flex justify-content-center align-items-center"
+                        style={{
+                          height: '400px',
+                          width: '504px',
+                          backgroundColor: 'var(--color-light-gray)',
+                          borderRadius: '50px',
+                        }}
+                      >
+                        {v}
+                      </div>
                     </div>
-                  </div>
-                )
-              })}
+                  )
+                })}
             </div>
             <button
               className={`${styles.hideBtn} carousel-control-prev`}
