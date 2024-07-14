c3d606e7a5c8ad0608c114b5572949dca39efd7e
sophie0318 <solophie34@gmail.com>
2024-07-14 22:03:29 +0800

    frontend 文章卡片加上愛心

--- frontend/components/articles/article-card/index.js
+++ frontend/components/articles/article-card/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
+import { IoHeart } from 'react-icons/io5'
 import styles from './article-card.module.css'
 
 const ArticleCard = ({
@@ -9,15 +10,32 @@ const ArticleCard = ({
   imgSrc = '/defaultImg.png',
   hrefURL = '/',
 }) => {
+  const [isClicked, setIsClicked] = useState(false)
+  const handleClick = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setIsClicked(!isClicked)
+    console.log('heart clicked')
+  }
+
   return (
     <div className={styles.articleCard}>
       <Link href={hrefURL}>
         <div className={styles.cardMainInfo}>
-          <img
-            src={`/articles-img/${imgSrc}`}
-            alt="描述圖片內容"
-            className={styles.cardImg}
-          />
+          <div className={styles.cardImgContainer}>
+            <img
+              src={`/articles-img/${imgSrc}`}
+              alt="描述圖片內容"
+              className={styles.cardImg}
+            />
+            <button className={`${styles.heart}`} onClick={handleClick}>
+              <IoHeart
+                className={`${styles.heartIcon} ${
+                  isClicked ? styles.clicked : ''
+                }`}
+              />
+            </button>
+          </div>
           <div className={styles.cardInfo1}>
             <div className={styles.cardTitle}>{title}</div>
           </div>
