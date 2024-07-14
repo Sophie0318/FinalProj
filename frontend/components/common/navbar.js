adc58c3f0380eadbe81a147cf6ecfe27bdf3ff41
sophie0318 <solophie34@gmail.com>
2024-07-14 23:05:07 +0800

    frontend navbar 版面調整

--- frontend/components/common/navbar.js
+++ frontend/components/common/navbar.js
@@ -21,7 +21,6 @@ import { useState, useEffect } from 'react'
 import { useCart } from '@/hooks/product/use-cart'
 
 // TODO: header logo offsets when toggle offcanvas, 可以參考kacco
-// TODO: toggle button 會蓋住scrollbar, 也參考kacco
 // 也可以用看看 bootstrap offcanvas body scrollable
 export default function Navbar({ hideLogo = false }) {
   const { auth, logout } = useAuth()
