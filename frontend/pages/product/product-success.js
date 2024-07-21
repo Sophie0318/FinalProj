ba2e5baf95c88ef08e1f2cc066506c72743afb72
lulu <ic093162193@gmail.com>
2024-07-21 17:07:17 +0800

    付款後，導到付款完成後，購物車會清空

---
+++ frontend/pages/product/product-success.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from '@/styles/product-success.module.css'
+import Layout3 from '@/components/layout/layout3'
+import { useEffect } from 'react'
+
+export default function ProductSuccess() {
+  useEffect(() => {
+    // 清空 localStorage 裡的商品
+    localStorage.removeItem('shoppingCart') // 假設你的商品存儲在 'cartItems' 鍵裡
+  }, [])
+  return (
+    <div>
+      <Layout3>
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <div className={styles.success}>付款成功！</div>
+            <div className={styles.reminder}>記得要取貨喔～</div>
+            <div className={styles.reserveInfo}>
+              <div className={styles.infoRow}>
+                <div className={styles.label}></div>
+                <div className={styles.details}>
+                  <div className={styles.lessonInfo}>
+                    <br />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.btns}>
+              <button className={styles.btnBack}>檢視訂單</button>
+              <button className={styles.btnFin}>回到課程頁</button>
+            </div>
+          </div>
+        </div>
+      </Layout3>
+    </div>
+  )
+}
