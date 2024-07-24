8ff52857ba560ab6282a78e2ac16f3845b32635c
lulu <ic093162193@gmail.com>
2024-07-25 05:46:31 +0800

    調整RWD

--- frontend/components/product/checkout3-order/checkout3-order.js
+++ frontend/components/product/checkout3-order/checkout3-order.js
@@ -90,8 +90,6 @@ export default function Checkout3Order() {
             marginTop: '20px',
             color: '#1A394A',
             fontSize: '25px',
-            // display: 'flex',
-            // justifyContent: 'center',
           }}
         >
           訂單明細
@@ -107,7 +105,8 @@ export default function Checkout3Order() {
             下訂日期: {orderDetail[0].orderDetail_time}
           </p>
         </div>
-        <p
+        <div
+          className="col-12 col-md-12"
           style={{
             fontSize: '20px',
             borderTop: '1px solid #1A394A',
@@ -115,74 +114,59 @@ export default function Checkout3Order() {
           }}
         >
           訂單編號:{orderDetail[0].orderDetail_number}
-        </p>
-        {/* ------------------------------------- */}
-        {/* <div className={styles.place}>
-          <span style={{ fontSize: '25px' }}>商品:</span>
-          <span style={{ fontSize: '25px' }}>價錢:</span>
         </div>
-        {orderDetail.map((v, i) => (
-          <div key={i} className={styles.place}>
-            <span>
-              {v.Product_name}x{v.OrdersDetail_product_quantity}
-            </span>
-            <span>{v.OrdersDetail_unit_price_at_time}</span>
-          </div>
-        ))} */}
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">商品</th>
-              <th scope="col">價格</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orderDetail.map((v, i) => (
-              <tr key={i}>
-                <th scope="row">{i + 1}</th>
-                <td>
-                  {v.Product_name} x {v.OrdersDetail_product_quantity}
-                </td>
-                <td>{v.OrdersDetail_unit_price_at_time}</td>
+        <div>
+          <table className="table" style={{ backgroundColor: '#FFF7E9 ' }}>
+            <thead>
+              <tr>
+                <th scope="col" style={{ backgroundColor: '#FFF7E9 ' }}>
+                  {/* # */}
+                </th>
+                <th scope="col" style={{ backgroundColor: '#FFF7E9 ' }}>
+                  商品
+                </th>
+                <th scope="col" style={{ backgroundColor: '#FFF7E9 ' }}>
+                  價格
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        {/* ------------------------------- */}
-
-        {/* <p style={{ fontSize: '25px' }}>
-          照片{' '}
-          <img
-            src={`../../product-img/${firstPhoto}`} // 確保這裡的路徑正確
-            alt="Product"
-            style={{ width: '100px', height: '100px' }} // 根據需要調整圖片大小
-          />
-        </p> */}
-        <div className={styles.subtotal}>
-          <span style={{ fontSize: '25px' }}>總數量:</span>
-          <span style={{ fontSize: '25px' }}>{calcTotalQty()}</span>
-          <span style={{ fontSize: '25px' }}>總金額:</span>
-          <span style={{ fontSize: '25px' }}>
-            NT$
-            {orderDetail.reduce(
-              (acc, item) =>
-                acc +
-                item.OrdersDetail_product_quantity *
-                  item.OrdersDetail_unit_price_at_time,
-              0
-              //orderDetail 是包含訂單項目的數組。
-              // reduce 方法對數組中的每個元素執行提供的回調函數（這裡是 (acc, item) => acc + item.qty），將每次回調函數的返回值累積到一個累加器（acc）。
-              // acc 是累加器，初始值為 0（即 reduce 方法的第二個參數 0）。
-              // item 是數組中的當前元素。
-              // acc + item.qty 將當前元素的 qty（商品數量）加到累加器中。
-              // 這樣，最終 reduce 方法返回累加器的值，即所有商品的總數量。
-            )}
-          </span>
+            </thead>
+            <tbody>
+              {orderDetail.map((v, i) => (
+                <tr key={i}>
+                  <th scope="row" style={{ backgroundColor: '#FFF7E9 ' }}>
+                    {i + 1}
+                  </th>
+                  <td style={{ backgroundColor: '#FFF7E9 ' }}>
+                    {v.Product_name} x {v.OrdersDetail_product_quantity}
+                  </td>
+                  <td style={{ backgroundColor: '#FFF7E9 ' }}>
+                    {v.OrdersDetail_unit_price_at_time}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-12 col-md-12">
+          <div className={`d-flex justify-content-between ${styles.subtotal}`}>
+            <span style={{ fontSize: '20px' }}>總數量:</span>
+            <span style={{ fontSize: '20px' }}>{calcTotalQty()}</span>
+            <span style={{ fontSize: '20px' }}>總金額:</span>
+            <span style={{ fontSize: '20px' }}>
+              NT$
+              {orderDetail.reduce(
+                (acc, item) =>
+                  acc +
+                  item.OrdersDetail_product_quantity *
+                    item.OrdersDetail_unit_price_at_time,
+                0
+              )}
+            </span>
+          </div>
         </div>
       </div>
-      <div className="col-12 col-md-6 mt-5 h6 text-center">
-        選擇付款方式
+      <div className={`col-12 col-md-6 mt-5 h6 text-center ${styles.zzz}`}>
+        <p className={styles.aa}>選擇付款方式</p>
         <div>
           <select
             id=""
@@ -195,7 +179,6 @@ export default function Checkout3Order() {
               borderRadius: '50px',
               marginTop: '30px',
               marginBottom: '20px',
-              marginLeft: '30px',
             }}
           >
             <option className="text-center">請選擇</option>
